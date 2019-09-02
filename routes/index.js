d2dafa5042ca872c026c554aa37ea7d126e9094c
shier <dingzhiwei@takecloud.cn>
2019-09-02 18:40:28 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
 
 router.get('/show', async (req, res, next) => {
   try {
-    let result = await require('../services/users').show();
+    let result = await require('../service/user').show();
     res.send(result);
   } catch (e) {
     res.send(e);
