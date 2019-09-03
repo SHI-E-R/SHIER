ab15ad0153a258192f5775e6480758537cc88806
shier <dingzhiwei@takecloud.cn>
2019-09-03 17:12:02 +0800

    fix

--- service/user.js
+++ service/user.js
@@ -4,21 +4,23 @@ var app = express();
 var connection = require('../config/db') 
 var conn = connection.connection
 
-conn.connection.connect((err) => {
-    if (err) {
-        console.log(err, "数据库连接失败");
-        return;
-    }
-    console.log('数据库连接成功');
+conn.connection();
 
-    app.listen(port, function (err) {
-        if (err) {
-            console.error('err:', err);
-        } else {
-            console.info(`===> api server is running at localhost:3306`)
-        }
-    });
-});
+// conn.connection.connect((err) => {
+//     if (err) {
+//         console.log(err, "数据库连接失败");
+//         return;
+//     }
+//     console.log('数据库连接成功');
+
+//     app.listen(port, function (err) {
+//         if (err) {
+//             console.error('err:', err);
+//         } else {
+//             console.info(`===> api server is running at localhost:3306`)
+//         }
+//     });
+// });
 
 //设置跨域访问
 app.all('*', function (req, res, next) {
