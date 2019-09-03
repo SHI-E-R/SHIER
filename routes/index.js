28a86f3bc958f9ac68f4370f9a83f5ac33f13a12
shier <dingzhiwei@takecloud.cn>
2019-09-03 16:47:03 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
-
-
-//设置跨域访问
-app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
-});
 
 /* GET home page. */
-app.get('/', function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'Express'
   });
 });
 
-app.get('/show', async (req, res, next) => {
+router.get('/show', async (req, res, next) => {
   try {
     let result = await require('../service/user').show();
     res.send(result);
@@ -28,7 +17,7 @@ app.get('/show', async (req, res, next) => {
   }
 })
 
-app.get('/test', async (req, res, next) => {
+router.get('/test', async (req, res, next) => {
   try {
     let result = await require('../service/user').test();
     res.send(result);
