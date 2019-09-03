4c00406a89dbcb24c45ba577e2a03afd9c8bf52b
shier <dingzhiwei@takecloud.cn>
2019-09-03 17:06:03 +0800

    fix

--- service/user.js
+++ service/user.js
@@ -2,24 +2,23 @@ var express = require('express');
 var router = express.Router();
 var app = express();
 var connection = require('../config/db') 
+var conn = connection.connection
 
-connection.connect();
+conn.connection.connect((err) => {
+    if (err) {
+        console.log(err, "数据库连接失败");
+        return;
+    }
+    console.log('数据库连接成功');
 
-// connection.connect((err) => {
-//     if (err) {
-//         console.log(err, "数据库连接失败");
-//         return;
-//     }
-//     console.log('数据库连接成功');
-
-//     app.listen(port, function (err) {
-//         if (err) {
-//             console.error('err:', err);
-//         } else {
-//             console.info(`===> api server is running at localhost:3306`)
-//         }
-//     });
-// });
+    app.listen(port, function (err) {
+        if (err) {
+            console.error('err:', err);
+        } else {
+            console.info(`===> api server is running at localhost:3306`)
+        }
+    });
+});
 
 //设置跨域访问
 app.all('*', function (req, res, next) {
@@ -32,7 +31,7 @@ app.all('*', function (req, res, next) {
 
 let show = () => {
     return new Promise((resolve, reject) => {
-        connection.query('select * from user', (err, rows) => {
+        conn.query('select * from user', (err, rows) => {
             if (err) {
                 reject(err);
                 console.log('出错了哦');
