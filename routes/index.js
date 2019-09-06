a887b02d81cf6b0c9c88db0053a5f3692ad173f5
shier <dingzhiwei@takecloud.cn>
2019-09-06 11:37:41 +0800

    feat：测试添加接口

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,15 @@ router.get('/show', async (req, res, next) => {
   }
 })
 
+router.post('/addArticle', async (req, res, next) => {
+  try {
+    const param = req.params
+    console.log(param)
+    let result = await api.show(param.title, param.remake);
+    res.send(result);
+  } catch (e) {
+    res.send('出错了没有try到');
+  }
+})
+
 module.exports = router;
