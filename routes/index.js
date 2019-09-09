634579091b7aa8bc0d628e6d841dd27971f36030
shier <dingzhiwei@takecloud.cn>
2019-09-09 16:16:55 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var api = require('../service/user')
-// let qiniu = require('qiniu'); // 需要加载qiniu模块的
-// const accessKey = 'AeJ3AwFOKhGdPxsRd-RL4wud3aq7at56flGi-GbK';
-// const secretKey = 'C2NmkNdGOcx5Qb22BcO2ys9U5pucnvrGinDCJ0oL';
-// const bucket = 'shier';
+let qiniu = require('qiniu'); // 需要加载qiniu模块的
+const accessKey = 'AeJ3AwFOKhGdPxsRd-RL4wud3aq7at56flGi-GbK';
+const secretKey = 'C2NmkNdGOcx5Qb22BcO2ys9U5pucnvrGinDCJ0oL';
+const bucket = 'shier';
 
 
 /* GET home page. */
@@ -14,20 +14,20 @@ router.get('/', function (req, res, next) {
   });
 });
 
-// router.post('/token', async(ctx, next)=> {
-//   let mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
-//   let options = {
-//       scope: bucket,
-//       expires: 3600 * 24
-//   };
-//   let putPolicy =  new qiniu.rs.PutPolicy(options);
-//   let uploadToken= putPolicy.uploadToken(mac);
-//   if (uploadToken) {
-//       ctx.body = Code('re_success', uploadToken)
-//   } else {
-//       ctx.body = Code('re_error')
-//   }
-// })
+router.post('/token', async(ctx, next)=> {
+  let mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+  let options = {
+      scope: bucket,
+      expires: 3600 * 24
+  };
+  let putPolicy =  new qiniu.rs.PutPolicy(options);
+  let uploadToken= putPolicy.uploadToken(mac);
+  if (uploadToken) {
+      ctx.body = Code('re_success', uploadToken)
+  } else {
+      ctx.body = Code('re_error')
+  }
+})
 
 router.get('/show', async (req, res, next) => {
   try {
